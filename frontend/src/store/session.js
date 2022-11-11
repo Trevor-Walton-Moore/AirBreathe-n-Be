11bfcdf9b739a948819aa7d9f73f4a89851635fa
Trevor WM <Trevormoore.2016@gmail.com>
2022-11-10 23:20:57 -0800

    login & restore components

---
+++ frontend/src/store/session.js
@@ -0,0 +1,61 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+    console.log('setUser ACTION')
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  console.log('login THUNK')
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  console.log('about to DISPATCH')
+  dispatch(setUser(data));
+  return response;
+};
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data));
+    return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
