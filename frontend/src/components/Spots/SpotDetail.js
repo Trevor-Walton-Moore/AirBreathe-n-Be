ea7773fe61243e2d9a53512c267b6acff80339d9
Trevor WM <Trevormoore.2016@gmail.com>
2022-11-20 03:01:25 -0800

    styled spot detail page

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -20,12 +20,16 @@ const SpotDetail = () => {
 
     const { spotId } = useParams();
 
+
     const spot = useSelector(state => state.spots[spotId]);
     const reviews = useSelector(state => state.reviews.spotReviewsArr);
+
+    // const ownerName = useSelector(state => state.session.user.firstName)
 
     let existingReview;
     let totalStars = 0;
     let avgRating = 0;
+    let totalReviews = 0;
 
     if (reviews && sessionUser) {
         for (let review of reviews) {
@@ -38,6 +42,7 @@ const SpotDetail = () => {
     if (reviews) {
         for (let review of reviews) {
             totalStars += +review.stars;
+            totalReviews += 1;
         }
         avgRating = totalStars / reviews.length;
     }
@@ -70,12 +75,72 @@ const SpotDetail = () => {
         // (sessionUser) &&
         <div className="spotMain">
             <div className="spotDisplay">
+                <div className='infoAboveImage'>
+                    <div className='spotName'>
+                        {spot.name}
+                    </div>
+                    <div>
+                        {avgRating ? <span className="spotText">
+                            <i class="fa-solid fa-star"></i>
+                            {avgRating}&nbsp;·&nbsp;
+                        </span> : ''}
+                        <span className='spotText'>
+                            {totalReviews} reviews&nbsp;·&nbsp;
+                        </span>
+                        <span className='spotInfo'>
+                            <span className="spotText">
+                                {spot.city}, {spot.state}, {spot.country}
+                            </span>
+                        </span>
+                    </div>
+                </div>
                 <img src={spot.previewImage} className='spotImage' alt='preview' />
-                <div className="spotDetail">
-                    {avgRating ? <h2 className='spotInfo'>{spot.name} ⭐️{avgRating}</h2> : ''}
-                    <div className='spotInfo'>address: {spot.address} {spot.city} {spot.state} {spot.country}</div>
-                    <p className='spotInfo'>price: ${spot.price}/night</p>
-                    <p className='spotInfo'>description: {spot.description}</p>
+                <div className="infoBelowImage">
+                    <span className="hostedBy">Home hosted by an owner</span>
+                    <div className="guestBedBath">
+                        4 guests&nbsp;·&nbsp;2 bedrooms&nbsp;·&nbsp;3 beds&nbsp;·&nbsp;2 baths
+                    </div>
+                    <div className="line"></div>
+                    <div className="perksContainer">
+                        <div className='perk'>
+                            <div className='left'>
+                                <i class="fa-solid fa-desktop icon"></i>
+                            </div>
+                            <div>
+                                <div>
+                                    Dedicated workspace
+                                </div>
+                                <div className="perksGreyed">
+                                    A common area with wifi that's well-suited for working.
+                                </div>
+                            </div>
+                        </div>
+                        <div className='perk'>
+                            <div className='left'>
+                                <i class="fa-solid fa-door-open icon"></i>
+                            </div>
+                            <div >
+                                <div>
+                                    Self check-in
+                                </div>
+                                <div className="perksGreyed">
+                                    Check yourself in with the keypad.
+                                </div>
+                            </div>
+                        </div>
+                        <div className='perk'>
+                            <div className='left'>
+                                <i class="fa-regular fa-calendar icon"></i>
+                            </div>
+                            <div>
+                                &nbsp;&nbsp;Free cancellation for 48 hours
+                            </div>
+                        </div>
+                    </div>
+                    {/* <p className='spotInfo'>price: ${spot.price}/night</p> */}
+                    <div className='line'></div>
+                    <p className='spotInfo'>{spot.description}</p>
+                    <div className='line'></div>
                 </div>
             </div>
             {
@@ -91,8 +156,8 @@ const SpotDetail = () => {
                         <NavLink to={`/spots/${spotId}/edit`} style={{ textDecoration: 'none' }}>
                             {/* <button className="button" style={{ visibility: hidden ? 'visible' : 'hidden' }} onClick={() => showForm()}>
                                 <span className="submit"> */}
-                                    Edit spot
-                                {/* </span>
+                            Edit spot
+                            {/* </span>
                             </button> */}
                         </NavLink>
                         {/* <div style={{ visibility: hidden ? 'hidden' : 'visible' }}>
